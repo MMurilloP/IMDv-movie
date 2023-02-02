7d29fc89eeac638d337a2290c41315f0154e1074
Manuel Murillo <mmpeira@gmail.com>
2023-02-02 10:56:20 +0100

    validacion correo y pass, vistas de index y logout

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const pool = require('./utils/db');
 const bcrypt = require('bcrypt');
-const session = require('express-session');
-
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 
 const port = 3000;
@@ -12,13 +12,6 @@ const app= express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
-
-// Use express-session middleware
-app.use(session({
-  secret: 'secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
 
 
 // Template engine
@@ -52,10 +45,21 @@ app.post('/register', async (req, res) => {
   const { username, email, password, role } = req.body;
   console.log(req.body);
 
+
+  // Verificar si el email es válido
+  if (!validator.isEmail(email)) {
+    return res.status(400).send('Por favor, ingrese un correo electrónico válido.');
+  }
+
+  // Verificar si la contraseña cumple con los requisitos
+  if (!validator.isLength(password, { min: 8 })) {
+    return res.status(400).send('La contraseña debe tener al menos 8 caracteres.');
+  }
+
   // Verificar si el usuario ya existe en la base de datos
   const existingUser = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
   if (existingUser.rows.length > 0) {
-    return res.status(400).send('Ese correo electronico ya esta registrado.');
+    return res.status(400).send('Ese correo electrónico ya esta registrado.');
   }
 
   // Hashear la contraseña
@@ -72,19 +76,30 @@ app.post('/register', async (req, res) => {
   });
 });
 
+
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   // Buscar al usuario en la base de datos
   const user = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
   if (user.rows.length === 0) {
-    return res.status(400).send('Ese correo electrónico no está registrado.');
+    return res.send(`
+      <script>
+        alert("Ese correo electrónico no está registrado.");
+        history.back();
+      </script>
+    `);
   }
 
   // Verificar la contraseña
   const isPasswordCorrect = await bcrypt.compare(password, user.rows[0].password);
   if (!isPasswordCorrect) {
-    return res.status(400).send('Contraseña incorrecta.');
+    return res.send(`
+      <script>
+        alert("Contraseña incorrecta.");
+        history.back();
+      </script>
+    `);
   }
 
   // Iniciar sesión (por ejemplo, guardar en una cookie el ID del usuario)
